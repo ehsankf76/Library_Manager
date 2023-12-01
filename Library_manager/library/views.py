f13c5db343b9677be3f3d602f3fe685fb5758512
Ehsan Karegar Foroogh <ehsan.kf76@gmail.com>
2023-12-01 14:06:12 +0330

    Write the basic of the model serializers

--- Library_manager/library/views.py
+++ Library_manager/library/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from . import models
+from django.shortcuts import get_object_or_404
+from . import serializers
+from rest_framework import viewsets
+
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing author instances.
+    """
+    queryset = models.Author.objects.all()
+    serializer_class = serializers.AuthorSerializer
+    lookup_field = 'slug'
+
+class PublisherViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing publisher instances.
+    """
+    queryset = models.Publisher.objects.all()
+    serializer_class = serializers.PublisherSerializer
+    lookup_field = 'slug'
+
+class GenreViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing genre instances.
+    """
+    queryset = models.Genre.objects.all()
+    serializer_class = serializers.GenreSerializer
+    lookup_field = 'slug'
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing book instances.
+    """
+    queryset = models.Book.objects.all()
+    serializer_class = serializers.BookSerializer
+    lookup_field = 'slug'
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing transaction instances.
+    """
+    queryset = models.Transaction.objects.all()
+    serializer_class = serializers.TransactionSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing review instances.
+    """
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
